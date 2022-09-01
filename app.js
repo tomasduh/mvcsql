29f7dbc8d52549c5f60178ebfef499b489c4e6bd
tomasduh <tomasduh421@gmail.com>
2022-09-01 15:28:15 -0500

    commit app conexion

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 
+const conexion = require('./database/db');
+
 app.get('/',(req,res)=>{
     res.sendFile('./views/index.html', {
         root:__dirname
     });
+})
+
+app.get('/conexion', (req, res)=>{
+    conexion.query('Select * from productos', (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    }
+    )
 })
 
 app.listen(3000);

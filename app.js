df81968538966c6485baffa6d93ce67c7a21b109
d4niel <olayajuandaniel8@gmail.com>
2022-09-01 15:53:26 -0500

    Query

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const conexion = require('./database/db');
 
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.sendFile('./views/index.html', {
         root:__dirname
@@ -20,6 +21,23 @@ app.get('/conexion', (req, res)=>{
     )
 })
 
+
+app.post('/create', (req,res)=>{
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+
+    conexion.query("INSERT INTO `productos`( `nombre`, `precio`) VALUES ("+nombre+","+precio+")",(error, result)=>{
+        if(error){
+            throw error;
+        }
+        else{
+            res.send(result);
+        }
+
+    }
+    )
+    // res.send('enviado');
+})
 app.listen(3000);
 
 console.log('server colocado');
